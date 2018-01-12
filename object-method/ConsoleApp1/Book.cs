95b0415c19ea6c4e3b91e409798a5ead5a2fccde
caiusnadcode <caius.fritz@outlook.com>
2018-01-12 11:13:03 +0200

    I was started to make book class and method for book class

---
+++ object-method/ConsoleApp1/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TaskBook
+{
+    class Book
+    {
+        public string Title;
+        private readonly string _author;
+        private int _id;
+        private double _price;
+
+        // Consructor
+        public Book()
+        {
+            Title = "Undefined";
+            _author = "Undefined";
+            _id = 0;
+            _price = 0;
+        }
+
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            _author = author;
+            _id = id;
+            _price = price;
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (_price > book._price)
+                return $"Kirja {Title} on kalliimpi kuin {book.Title} kirja.";
+            else
+                return string.Format("Kirja {0} on kallimpi kuin {1} kirja.", book.Title, Title);
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            var result = $"Kirjan nimi:\t{Title}\n";
+            return result;
+
+         }
+
+
+    }
+}
+
+
+
---
+++ object-method/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+
+namespace TaskBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Book book2 = new Book("C#","Masa",123,22.90);
+            Book book3 = new Book("Java", "Jim", 3456, 30.90);
+
+        }
+            
+}
+}
+      
