7c8ddf3e269fdb2a96bec189dfc6ab1924323d3b
Caiusandcode <34799763+Caiusandcode@users.noreply.github.com>
2018-01-24 00:06:56 +0200

    Add files via upload
    The first object program is now created. Little bit i checked how others had made it, need to say.

---
+++ Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+
+namespace Taskbooknew
+
+{
+
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+
+        //Constructor
+        public Book()
+        {
+            Title = "Undefined";
+            Author = "Undefined";
+            Id = 0;
+            Price = 0;
+        }
+
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+
+        public string CompareBook(Book kirja)
+        {
+            if (Price > kirja.Price)
+                return $"{Title} kirja on kalliimpi kuin {kirja.Title} kirja. \n";
+
+            return $"{kirja.Title} kirja on kalliimpi kuin {Title} kirja. \n";
+
+        }
+
+        public bool IsExpensier(Book kirja)
+        {
+            return (Price > kirja.Price);
+        }
+
+     
+        
+    
+        
+        // Methods
+        public override string ToString()
+        {
+            var result = $"Kirjan nimi:\t{Title}\n" +
+                         $"Kirjoittaja:\t{Author}\n" +
+                         $"Id:\t\t{Id}\n" +
+                         $"Kappalehinta:\t{Price} \n";
+
+            return result;
+        }
+        
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Taskbooknew
+{
+    class Program
+    {
+        
+            
+        static void Main(string[] args)
+        { 
+            Book newBook = new Book();
+            
+            Book book2 = new Book();
+            book2.Price = 15.50;
+            book2.Author = "Kiiskinen";
+            book2.Title = "Rakennuskirja";
+            book2.Id = 4000;
+
+            Book book3 = new Book("LVI Asennus", "Heikki", 25, 11.00);
+            Book book4 = new Book("Raketti tiede", "Olavi Kiiski", 3, 40.00);
+            Console.WriteLine(book2.ToString());
+            Console.WriteLine(book3.ToString());
+            Console.WriteLine(book4.ToString());
+            Console.WriteLine(book2.CompareBook(book4));
+            
+            
+            
+            Console.ReadKey();
+
+        }
+        
+    }
+}
+    
+
