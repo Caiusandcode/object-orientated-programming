871bd74085ecf99f7c2b52648981261ddaa8dc34
Caiusandcode <34799763+Caiusandcode@users.noreply.github.com>
2018-01-25 11:56:54 +0200

    Add files via upload
    There was first clerical errors in my code. Now solved and nice loop also inside the  program.

---
+++ object-method/Firma.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFirma
+{
+    class Firma
+    {
+        //Fields, attributes
+        public string Name;
+        public string Adress;
+        public string Phone;
+        public double Outcome;
+        public double Income;
+
+
+        //Constructor
+        //public Firma()
+        //{
+        //    Name = "unknown";
+        //    Adress = "unknown";
+        //    Phone = 0;
+        //    Outcome = 0;
+        //    Income = 0;
+
+        //}
+
+        public Firma(string name, string adress, string phone, int outcome, int income)
+        {
+            Name = name;
+            Adress = adress;
+            Phone = phone;
+            Outcome = outcome;
+            Income = income;
+
+        }
+        public Firma()
+        {
+            Name = "TakuuAuto";
+            Adress = "Kivitie 37 00700 Malmi, Helsinki";
+            Phone = "0200 770 990";
+            Outcome = 530400;
+            Income = 681500;
+        }
+
+        // Copy constructor.
+        public Firma(Firma previousFirma)
+        {
+            Name = previousFirma.Name;
+            Adress = previousFirma.Adress;
+            Phone = previousFirma.Phone;
+            Outcome = previousFirma.Outcome;
+            Income = previousFirma.Income;
+        }
+
+        //Methods
+
+        public void ProfitOfCompany()
+        {
+            double profit = (Income - Outcome) / Outcome * 100;
+
+            if (profit <= 100)
+                Console.WriteLine($"Firmalla {Name} menee kehnosti.");
+            else if (profit > 100 && profit <= 200)
+                Console.WriteLine($"Firmalla {Name} menee tyydyttävästi");
+            else
+                Console.WriteLine($"Firmalla {Name} menee hyvin");
+        }
+
+        public override string ToString()
+        {
+            string result = $"------------------------\n" +
+                 $"Firman nimi:\t{Name}\n" +
+                 $"Osoite:\t{Adress}\n" +
+                 $"Puhelin:\t\t{Phone}\n" +
+                 $"Menot:\t{Outcome}\n" +
+                 $"Tulot:\t{Income}\n" +
+                 $"======================\n";
+
+
+            return result;
+        }
+    }
+}
+
--- object-method/Program.cs
+++ object-method/Program.cs
@@ -1,33 +1,31 @@
 ﻿using System;
 
-namespace TaskCar
+namespace TaskFirma
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Car newCar = new Car();
-            newCar.AskData();
-            newCar.ShowCarInfo();
-            newCar.Accelerate();
-            newCar.ShowCarInfo();
-            newCar.Brake();
-            newCar.ShowCarInfo();
 
-            Car car2 = new Car("BMW",150);
-            car2.ShowCarInfo();
-            car2.Accelerate();
-            car2.ShowCarInfo();
-            car2.Brake();
-            car2.ShowCarInfo();
+            Firma[] companies = new Firma[3];
+
+            companies[0] = new Firma("Kartonkitehdas", "Tikkapolku 4, Vantaa", "020 0688 433", 1200400, 870000);
+            companies[1] = new Firma("Konsultti firma", "Hiihtotie 2 Tampere", "020 0500 600", 250100, 440600);
+            companies[2] = new Firma("Tekniikan ihme", "Onnelantie 8 Espoo", "020 572 042", 2260500, 10560210);
+
+            
+
+            for(int i=0;i<companies.Length;i++)
+            {
+                Console.WriteLine(companies[i].ToString());
+                companies[i].ProfitOfCompany();
+                
+            }
+
+            
 
 
-            Car car3 = new Car("Skoda", 80);
-            car3.ShowCarInfo();
-            car3.Accelerate();
-            car3.ShowCarInfo();
-            car3.Brake();
-            car3.ShowCarInfo();
+
 
 
             Console.ReadKey();
