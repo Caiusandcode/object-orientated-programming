ad3d1dfd88dd80597fef22a3e601757a49557bea
Caiusandcode <34799763+Caiusandcode@users.noreply.github.com>
2018-01-25 11:17:15 +0200

    Add files via upload
    Brake and Accelerate methods first didn't work, but now perfect

---
+++ object-method/ClassCar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCar
+{
+    class Car
+    {
+        public string Brand;
+        public double Speed;
+
+        // Constructor
+
+        public Car()
+        {
+            Brand = "Undefined";
+            Speed = 0;
+
+        }
+
+        public Car(string brand, int speed)
+        {
+
+            Brand = brand;
+            Speed = speed;
+        }
+
+        //Methods 
+
+
+
+        public void AskData()
+        {
+            Console.WriteLine("Please enter the name of a car.");
+            Brand = Console.ReadLine();
+            Console.WriteLine("Please enter the speed of a car.");
+            Speed = double.Parse(Console.ReadLine());           
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Malli: {Brand}\n" +
+                $"Nopeus: {Speed}");
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Please give a new speed");
+            double newspeed = double.Parse(Console.ReadLine());
+            Speed = Speed + newspeed;
+            Console.WriteLine("Kiihdytetään");
+        }
+
+
+        public void Brake()
+        {
+            Speed = Speed * 0.9;
+            Console.WriteLine("Jarrutetaan");
+        }
+
+    }
+}
+
---
+++ object-method/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TaskCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car();
+            newCar.AskData();
+            newCar.ShowCarInfo();
+            newCar.Accelerate();
+            newCar.ShowCarInfo();
+            newCar.Brake();
+            newCar.ShowCarInfo();
+
+            Car car2 = new Car("BMW",150);
+            car2.ShowCarInfo();
+            car2.Accelerate();
+            car2.ShowCarInfo();
+            car2.Brake();
+            car2.ShowCarInfo();
+
+
+            Car car3 = new Car("Skoda", 80);
+            car3.ShowCarInfo();
+            car3.Accelerate();
+            car3.ShowCarInfo();
+            car3.Brake();
+            car3.ShowCarInfo();
+
+
+            Console.ReadKey();
+        }
+    }
+}
