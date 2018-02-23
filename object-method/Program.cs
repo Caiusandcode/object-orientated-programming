5b734bd75a709e7571a8d85b2e22224bae223d7e
Caiusandcode <34799763+Caiusandcode@users.noreply.github.com>
2018-02-23 11:56:06 +0200

    Add files via upload

--- object-method/Program.cs
+++ object-method/Program.cs
@@ -1,63 +1,38 @@
 ﻿using System;
 
-namespace Task_lippu
+namespace TaskCar
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter customer data...");
+            Car newCar = new Car();
+            newCar.AskData();
+            newCar.ShowCarInfo();
+            newCar.Accelerate();
+            newCar.ShowCarInfo();
+            newCar.Brake();
+            newCar.ShowCar2();
 
-            Console.Write("Enter name  :  ");
+            Car car2 = new Car("BMW",150);
+            car2.ShowCarInfo();
+            car2.Accelerate();
+          
+            car2.ShowCarInfo();
+            car2.Brake();
+            car2.ShowCar2();
 
-            var name = Console.ReadLine();
-            var ticket = new Ticket(name);
 
-            int age = -1;
-            while (age < 0)
-            {
+            Car car3 = new Car();
+            car3.AskData();
+            car3.ShowCarInfo();
+            car3.Accelerate();
+            car3.ShowCarInfo();
+            car3.Brake();
+            car3.ShowCar2();
 
-                Console.Write("Enter age");
-                if (!int.TryParse(Console.ReadLine(), out age))
-                {
-                    Console.WriteLine("Invalid age!");
-                    age = -1;
-                }
-                else
-                {
 
-                    ticket.Age = age;
-                }
-            }
-
-            ticket.IsMtkmember = YesNo("Is customer a member of MTK");
-            ticket.IsConscript = YesNo("Is customer a conscript");
-            ticket.IsStudent = YesNo("Is customer a student");
-
-            Console.WriteLine($"Price for {ticket.Name} is {ticket.Whatprice():F2}");
             Console.ReadKey();
         }
-
-       
-
-        private static bool YesNo(string prompt)
-        {
-            while (true)
-            {
-                Console.Write("{0} (Y/N) : ", prompt);
-                char c = char.ToUpper(Console.ReadKey().KeyChar);
-                Console.WriteLine();
-                if (c == 'Y')
-                {
-                    return true;
-                }
-                if (c == 'N')
-                {
-                    return false;
-                }
-                Console.WriteLine("Please answer Y or N!");
-            }
-        }
     }
-}   
-
+}
