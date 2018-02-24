10ba4bbb34b3eb631626c82a52be5733553922c1
Caiusandcode <34799763+Caiusandcode@users.noreply.github.com>
2018-02-24 15:00:09 +0200

    Add files via upload

---
+++ object-method/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        //Muuttujat
+        public string Name;
+        public int Id;
+        public string Position;
+        public double Salary;
+
+        
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+
+        //Metodit
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"ID:\t\t{Id}\n" +
+                $"Titteli:\t{Position}\n" +
+                $"Nimi:\t\t{Name}\n" +
+                $"Palkka:\t\t{Salary}\n" +
+                $"----------------------");
+        }
+
+        public string CompareSalary(Employee worker)
+        {
+            if (Salary > worker.Salary)
+                return($"{Name} palkka on suurempi kuin {worker.Name} palkka.");
+            else
+                return($"{worker.Name} palkka on suurempi kuin {Name} palkka.");
+        }
+
+    }
+
+}
---
+++ object-method/Program.Empoyee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Masa", 25, "Boss", 7500);
+            employees[1] = new Employee("Ann", 10, "Advisor", 4600);
+            employees[2] = new Employee("Emilia", 46, "Developer",4400 );
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i].PrintEmployeeInfo();
+            }
+
+            Console.WriteLine($"{employees[0].CompareSalary(employees[1])}");
+            Console.WriteLine($"{employees[1].CompareSalary(employees[2])}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
